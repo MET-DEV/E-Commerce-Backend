2eb5138dc2a083fd449b4c749f8c94bb071ec9e0
MET-DEV <mustafatpe23@gmail.com>
2022-03-19 16:30:35 +0300

    CRUD operations completed for category

--- src/main/java/com/metsoft/ecommerce/api/controller/CategoriesController.java
+++ src/main/java/com/metsoft/ecommerce/api/controller/CategoriesController.java
@@ -3,11 +3,15 @@ package com.metsoft.ecommerce.api.controller;
 import com.metsoft.ecommerce.model.Category;
 import com.metsoft.ecommerce.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
+@RestController
 @RequestMapping("/api/categories/")
 public class CategoriesController {
     private CategoryRepository categoryRepository;
@@ -20,4 +24,43 @@ public class CategoriesController {
     public List<Category> getCategories(){
         return categoryRepository.findAll();
     }
+    @GetMapping("/getbyid/{id}")
+    public ResponseEntity getCategoryById(@PathVariable int id){
+        Optional<Category> category=categoryRepository.findById(id);
+        if (category.isEmpty()){
+            return  ResponseEntity.status(404).body("category not found");
+        }
+        return  ResponseEntity.ok(category);
+
+    }
+
+
+    @PostMapping("add")
+    public ResponseEntity saveCategory(@RequestBody Category category){
+        if (category.getId()!=0){
+            return ResponseEntity.badRequest().body("id is generated value. You cannot assign.");
+        }
+        return ResponseEntity.ok(categoryRepository.save(category));
+    }
+
+    @PatchMapping("update")
+    public ResponseEntity updateCategory(@RequestBody Category category) {
+        if(category.getId()==0){
+            return  ResponseEntity.badRequest().body("id is required");
+        }
+        return  ResponseEntity.ok(categoryRepository.save(category));
+
+    }
+    @DeleteMapping("delete")
+    public ResponseEntity deleteCategory(@RequestBody Category category){
+        if (category.getId()==0){
+            return  ResponseEntity.badRequest().body("id is required");
+        }
+        categoryRepository.deleteById(category.getId());
+        return ResponseEntity.ok("Deleted");
+
+    }
+
+
+
 }
--- src/main/java/com/metsoft/ecommerce/model/Category.java
+++ src/main/java/com/metsoft/ecommerce/model/Category.java
@@ -1,7 +1,6 @@
 package com.metsoft.ecommerce.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
